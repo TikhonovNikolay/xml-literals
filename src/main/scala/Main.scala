7b8bc28af0fae71ee6c6475b1f371d7fef2b9af4
tikhonovnicolay@gmail.com <098500dfktyrb>
2012-12-10 19:19:13 +0800

    Screening brackets

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,24 @@ package main.scala
 import xml.XML
 
 class XmlContext(sc: StringContext) {
+
+  def validateParameter(str : Any) = {
+    if (str.isInstanceOf[String]) {
+      str.asInstanceOf[String].replace("<", "&lt;").replace(">", "&gt;")
+    } else {
+      str
+    }
+  }
+
   def xml(args: Any*) = {
     sc.checkLengths(args)
     XML.loadString(
       args.zip(sc.parts).foldLeft("")
         ((x, y) => {
           if (y._2.endsWith("=") && (x + y._2).count(p => p.equals('<') || p.equals('>')) % 2 != 0) {
-            x + y._2 + "\"" + y._1 + "\""
+            x + y._2 + "\"" + validateParameter(y._1) + "\""
           } else {
-            x + y._2 + y._1
+            x + y._2 + validateParameter(y._1)
           }
         })
         + sc.parts.apply(sc.parts.length - 1)
@@ -26,7 +35,7 @@ object Main extends App {
   val V1 = 1
   val V2 = 2
   val V3 = 3
-  val V4 = 4
+  val V4 = "</a> 4 <a>"
   println( xml"""<html><testParameter par1=$V1 par2=$V2 par3=$V3 par4=$V4 /><body>There is no quote=$V1</body></html>""")
   val xs = List(1, 2, 3, 4, 5)
   println( xml"""<for>${(for (x <- xs) yield xml"""<bar x=$x/>""").foldLeft("")((x, y) => x + y)}</for>""")
