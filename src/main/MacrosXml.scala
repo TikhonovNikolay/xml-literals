f342f1a49b98b9422764b0f5895c304ce9e3ae1f
tikhonovnicolay <tikhonovnicolay@gmail.com>
2013-01-10 18:00:16 +0800

    Refactoring

--- src/main/MacrosXml.scala
+++ src/main/MacrosXml.scala
@@ -1,8 +1,7 @@
 package main
 
 import language.experimental.macros
-import reflect.macros.Context
-import scala.Predef
+import reflect.macros._
 
 object MacrosXml {
 
@@ -12,21 +11,14 @@ object MacrosXml {
 
     import c.universe._
 
-    val seq = reify(scala.collection.Seq).tree
-    val xmlUtil = reify(scala.xml.XML).tree
-    val scalaPreDef = reify(scala.Predef).tree
-
     c.Expr(
       Apply(
-        Select(xmlUtil, newTermName("loadString")),
+        Select(Select(Select(Ident(newTermName("scala")), newTermName("xml")), newTermName("XML")), newTermName("loadString")),
         List(
           Apply(
             Select(
               Apply(
-                Apply(
-                  Select(
-                    Apply(Apply(Select(Ident(newTermName("args")), newTermName("zip")), List(Select(sc.tree, newTermName("parts")))), List(Select(seq, newTermName("canBuildFrom")))),
-                    newTermName("foldLeft")), List(Literal(Constant("")))),
+                Apply(Select(Apply(Apply(Select(Ident(newTermName("args")), newTermName("zip")), List(Select(sc.tree, newTermName("parts")))), List(Select(reify(scala.collection.Seq).tree, newTermName("canBuildFrom")))), newTermName("foldLeft")), List(Literal(Constant("")))),
                 List(
                   Function(
                     List(ValDef(Modifiers(Flag.PARAM), newTermName("x"), TypeTree(), EmptyTree), ValDef(Modifiers(Flag.PARAM), newTermName("y"), TypeTree(), EmptyTree)),
@@ -41,7 +33,7 @@ object MacrosXml {
                                   Apply(
                                     Select(
                                       Apply(
-                                        Select(scalaPreDef, newTermName("augmentString")),
+                                        Select(reify(scala.Predef).tree, newTermName("augmentString")),
                                         List(Apply(Select(Ident(newTermName("x")), newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_2")))))
                                       ),
                                       newTermName("count")
@@ -50,43 +42,29 @@ object MacrosXml {
                                       Function(
                                         List(ValDef(Modifiers(Flag.PARAM), newTermName("p"), TypeTree(), EmptyTree)),
                                         Apply(
-                                          Select(Apply(Select(Ident(newTermName("p")), newTermName("equals")), List(Literal(Constant("<")))), newTermName("$bar$bar")),
-                                          List(Apply(Select(Ident(newTermName("p")), newTermName("equals")), List(Literal(Constant(">")))))
+                                          Select(Apply(Select(Ident(newTermName("p")), newTermName("equals")),
+                                            List(Literal(Constant("<")))), newTermName("$bar$bar")), List(Apply(Select(Ident(newTermName("p")), newTermName("equals")),
+                                            List(Literal(Constant(">")))))
                                         )
                                       )
                                     )
                                   ),
-                                  newTermName("$percent")
-                                ),
+                                  newTermName("$percent")),
                                 List(Literal(Constant(2)))
-                              ),
-                              newTermName("$bang$eq")
-                            ),
-                            List(Literal(Constant(0)))
+                              ), newTermName("$bang$eq")
+                            ), List(Literal(Constant(0)))
                           )
                         )
                       ),
                       Apply(
                         Select(
                           Apply(
-                            Select(
-                              Apply(
-                                Select(Apply(Select(Ident(newTermName("x")), newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_2")))),
-                                  newTermName("$plus")), List(Literal(Constant("\"")))
-                              ),
-                              newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_1")))
+                            Select(Apply(Select(Apply(Select(Ident(newTermName("x")), newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_2")))), newTermName("$plus")), List(Literal(Constant("\"")))), newTermName("$plus")),
+                            List(Select(Ident(newTermName("y")), newTermName("_1")))
                           ),
                           newTermName("$plus")
-                        ),
-                        List(Literal(Constant("\"")))),
-                      Apply(
-                        Select(
-                          Apply(Select(Ident(newTermName("x")), newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_2")))),
-                          newTermName("$plus")
-                        ),
-                          List(Select(Ident(newTermName("y")), newTermName("_1"))
-                        )
-                      )
+                        ), List(Literal(Constant("\"")))),
+                      Apply(Select(Apply(Select(Ident(newTermName("x")), newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_2")))), newTermName("$plus")), List(Select(Ident(newTermName("y")), newTermName("_1"))))
                     )
                   )
                 )
@@ -95,16 +73,13 @@ object MacrosXml {
             ),
             List(
               Apply(
-                Select(Select(sc.tree, newTermName("parts")), newTermName("apply")),
-                List(
-                  Apply(Select(Select(Select(sc.tree, newTermName("parts")), newTermName("length")), newTermName("$minus")), List(Literal(Constant(1))))
-                )
+                Select(Select(Ident(newTermName("sc")), newTermName("parts")), newTermName("apply")),
+                List(Apply(Select(Select(Select(sc.tree, newTermName("parts")), newTermName("length")), newTermName("$minus")), List(Literal(Constant(1)))))
               )
             )
           )
         )
       )
     )
-
   }
 }
--- src/main/Main.scala
+++ src/main/Main.scala
@@ -1,11 +1,12 @@
 package main
 
-import xml.XML
-import scala.reflect.runtime.universe._
+
 
 class XmlContext(sc: StringContext) {
 
-  def xml(args: Any*) = MacrosXml.xml(sc, args)
+  def xml(args: Any*) = {
+    RuntimeXml.xml(sc, args)
+  }
 
 }
 
@@ -13,8 +14,9 @@ object Main extends App {
 
   implicit def stringContextToXmlContext(sc: StringContext) = new XmlContext(sc)
 
-  val V1 = 1
+  val V1 = "444555"
   val V2 = 2
-  println(xml"""<html a = "$V2">$V1</html>""")
-  println(xml"""<html a = $V2>$V1</html>""")
+  val V3 = 3
+  val V4 = "444"
+  println( xml"""<html><testParameter par1="$V1" par2="$V2" par3="$V3" par4="$V4" /><body>There is no quote=$V1</body></html>""")
 }
